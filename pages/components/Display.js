8544821be6e84751a25f2a3ec680cbd057546e4c
Victory <vicnosa20@gmail.com>
2022-08-20 20:21:38 +0100

    successful Navbar

---
+++ pages/components/Display.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Display = () => {
+  return (
+    <div className="flex flex-col ">
+      <div className="text-[35px] text-white">350+450</div>
+      <span className="text-[45px] text-white opacity-[45%]">0</span>
+    </div>
+  );
+};
+
+export default Display;
