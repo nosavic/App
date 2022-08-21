9d8a56cfde6fa77dc39ccbb059c696072ae2610d
Victory <vicnosa20@gmail.com>
2022-08-21 19:56:22 +0100

    first

--- components/Digits.js
+++ components/Digits.js
@@ -2,51 +2,92 @@ import React from "react";
 
 const Digits = () => {
   return (
-    <div className=" relative text-[35px] text-white flex justify-between ">
-      <div className=" flex gap-[10px] justify-evenly flex-1 ml-4 mr-6 ">
-        <div className=" z-10   flex flex-col justify-evenly border-solid border-[4px] border-[#787878] rounded-[14px] pl-[7px] pr-[7px] gap-[12px]">
-          <button>7</button>
-          <button>4</button>
-          <button>1</button>
+    <div className="relative">
+      <div className="flex justify-between mr-[12px]">
+        <div className="flex w-[100%] justify-evenly">
+          <svg width="54" height="229" viewBox="0 0 54 229" fill="none">
+            <rect
+              x="2"
+              y="2"
+              width="50"
+              height="225"
+              rx="14"
+              stroke="#787878"
+              stroke-width="4"
+            />
+          </svg>
+          <svg width="54" height="229" viewBox="0 0 54 229" fill="none">
+            <rect
+              x="2"
+              y="2"
+              width="50"
+              height="225"
+              rx="14"
+              stroke="#787878"
+              stroke-width="4"
+            />
+          </svg>
+          <svg width="54" height="229" viewBox="0 0 54 229" fill="none">
+            <rect
+              x="2"
+              y="2"
+              width="50"
+              height="225"
+              rx="14"
+              stroke="#787878"
+              stroke-width="4"
+            />
+          </svg>
         </div>
-        <div className=" z-20 flex flex-col justify-evenly border-solid border-[4px] border-white rounded-[14px] pl-[7px] pr-[7px] gap-[12px]">
-          <button>8</button>
-          <button>5</button>
-          <button>2</button>
-        </div>
-        <div className=" z-10 flex ml-[2px] flex-col justify-evenly border-solid border-[4px] border-[#787878] rounded-[14px] pl-[7px] pr-[7px] gap-[12px] ">
-          <button>9</button>
-          <button>6</button>
-          <button>3</button>
+        <div>
+          <svg width="54" height="229" viewBox="0 0 54 229" fill="none">
+            <rect
+              x="2"
+              y="2"
+              width="50"
+              height="225"
+              rx="14"
+              stroke="#787878"
+              stroke-width="4"
+            />
+          </svg>
         </div>
       </div>
-      <div className="flex flex-col mr-4 border-solid border-[4px] border-[#787878] rounded-[14px] p-2">
-        <button>x</button>
-        <button>-</button>
-        <button>+</button>
-      </div>
-      <div className=" absolute w-[100%] h-[100%] top-0 text-[35px] text-white flex justify-between  ">
-        <div className=" gap-[10px] flex flex-col justify-evenly flex-1 ml-[30px] mr-[38px] ">
-          <div className="flex h-[40px] justify-evenly border-solid border-[4px] border-[#787878] rounded-[14px] ">
-            <button className="opacity-0">7</button>
-            <button className="opacity-0">4</button>
-            <button className="opacity-0">1</button>
-          </div>
-          <div className=" z-0 flex h-[40px] mt-[4px] justify-evenly border-solid border-[4px] border-white rounded-[14px] ">
-            <button className="opacity-0">8</button>
-            <button className="opacity-0">5</button>
-            <button className="opacity-0">2</button>
-          </div>
-          <div className="flex h-[40px] mt-[2px] justify-evenly border-solid border-[4px] border-[#787878] rounded-[14px]  ">
-            <button className="opacity-0">9</button>
-            <button className="opacity-0">6</button>
-            <button className="opacity-0">3</button>
-          </div>
-        </div>
-        <div className="flex flex-col mr-4 border-solid border-[4px] border-[#787878] rounded-[14px] p-2">
-          <button className="opacity-0">x</button>
-          <button className="opacity-0">-</button>
-          <button className="opacity-0">+</button>
+      <div className="absolute w-[100%]  top-0">
+        <div className=" flex flex-col gap-[36.5px]">
+          <svg width="236" height="52" viewBox="0 0 236 52" fill="none">
+            <rect
+              x="2"
+              y="2"
+              width="232"
+              height="48"
+              rx="14"
+              stroke="#787878"
+              stroke-width="4"
+            />
+          </svg>
+          <svg width="236" height="52" viewBox="0 0 236 52" fill="none">
+            <rect
+              x="2"
+              y="2"
+              width="232"
+              height="48"
+              rx="14"
+              stroke="#787878"
+              stroke-width="4"
+            />
+          </svg>
+          <svg width="236" height="52" viewBox="0 0 236 52" fill="none">
+            <rect
+              x="2"
+              y="2"
+              width="232"
+              height="48"
+              rx="14"
+              stroke="#787878"
+              stroke-width="4"
+            />
+          </svg>
         </div>
       </div>
     </div>
